f9d51d0f3ad6cef4c3ef7da91fcd33fd74e56b63
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-12 20:56:50 -0500

    removed console log

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,7 +10,6 @@ const displayMessage = function(message) {
 
 document.querySelector(`.check`).addEventListener(`click`, function() {
     const guess = Number(document.querySelector(`.guess`).value);
-    // console.log(guess, typeof guess);
 
     if(!guess) {
         // document.querySelector(`.message`).textContent = 
