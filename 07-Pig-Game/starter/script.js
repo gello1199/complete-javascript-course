ec09b062c3db698a8067635d67c953472b6d7de6
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-03-14 21:06:10 -0400

    removed init call

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -36,7 +36,7 @@ const init = function() {
     player1El.classList.remove(`player--active`);
 };
 
-init();
+
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
